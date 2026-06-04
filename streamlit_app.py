0e0318cab7ad9f9d51325118efc161a39d9e0272
Vincent Chen <vincentchen737@gmail.com>
2026-06-04 21:50:51 +0000

    Final fixes for sliders

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ st.write("dz/dt = xy-βz")
 
 st.sidebar.header("Parameters")
 sigma = st.sidebar.slider("σ (sigma)",  0.0,  20.0, 10.0, step=0.1)
-rho   = st.sidebar.slider("ρ (rho)",    0.0,  50.0, 25.0, step=0.1)
-beta  = st.sidebar.slider("β (beta)",   0.0,  10.0, 3.0, step=0.01)
+rho   = st.sidebar.slider("ρ (rho)",    0.0,  50.0, 26.0, step=0.1)
+beta  = st.sidebar.slider("β (beta)",   0.0,  10.0,  3.0, step=0.01)
 
 st.sidebar.header("Initial Conditions")
 x0 = st.sidebar.number_input("x₀",  value=1.0)
